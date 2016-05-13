a2847e864ba6224ccb074a0c59d20a82e4ce5a4b
Axel Dries <axel.dries@immalle.eu>
2016-05-13 12:49:34 +0200

    Basic Attack

---
+++ Fight/Monster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight
+{
+    class Monster
+    {
+        public int health;
+        public Random Attack = new Random();
+        public Random Heal = new Random();
+
+        public Monster()
+        {
+            health = 1000;
+        }
+
+        public void Do()
+        {
+            
+        }
+
+        public int Health
+        {
+            get { return health; }
+        }
+        
+
+    }
+}
---
+++ Fight/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var Monster1 = new Monster();
+            var Monster2 = new Monster();
+
+            Console.WriteLine(" U kan typen: Attack, Heal");
+            Console.WriteLine();
+
+            string key = Console.ReadLine();
+
+            if (key == "Attack")
+            {
+                int damage = Monster2.Attack.Next(1, 100);
+                Monster1.health = Monster1.health - damage;
+                Console.WriteLine("You've taken {0} damage", damage);
+                Console.WriteLine("You've {0} health remaining", Monster1.health);
+            }
+
+
+
+        }
+    }
+}
